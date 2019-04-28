4a3bce4a5fe5866187f1c44c3a0cdfac9eec4462
Jack Connolly <connoldh@bc.edu>
2019-04-28 12:56:53 -0400

    Initial Commit

---
+++ MortReport2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MortReport2
+//
+//  Created by John Connolly on 4/28/19.
+//  Copyright © 2019 John Connolly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
